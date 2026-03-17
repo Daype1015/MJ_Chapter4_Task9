16d3568d1c7d6997bb36f2b3e4d712d324fe6232
Daype <mj1527123@gmail.com>
2026-03-17 16:19:32 +0900

    게임시작시 화면에 UI 띄우기

--- NumberBaseBall/Source/NumberBaseBall/Player/NBPlayerController.cpp
+++ NumberBaseBall/Source/NumberBaseBall/Player/NBPlayerController.cpp
@@ -2,4 +2,24 @@
 
 
 #include "NBPlayerController.h"
+#include "NumberBaseBall/UI/NBGameWidget.h"
+#include "Blueprint/UserWidget.h"
 
+ANBPlayerController::ANBPlayerController()
+{
+	
+}
+
+void ANBPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (IsValid(PlayGameWidgetClass) == true)
+	{
+		PlayeGameWidgetInstance = CreateWidget<UNBGameWidget>(this, PlayGameWidgetClass);
+		if (IsValid(PlayeGameWidgetInstance) == true)
+		{
+			PlayeGameWidgetInstance->AddToViewport();
+		}
+	}
+}
--- NumberBaseBall/Source/NumberBaseBall/Player/NBPlayerController.h
+++ NumberBaseBall/Source/NumberBaseBall/Player/NBPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "NBPlayerController.generated.h"
 
+class UNBGameWidget;
 
 /**
  * 
@@ -14,5 +15,17 @@ UCLASS()
 class NUMBERBASEBALL_API ANBPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	ANBPlayerController();
+
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UNBGameWidget> PlayGameWidgetClass;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	TObjectPtr<UNBGameWidget> PlayeGameWidgetInstance;
 	
 };
